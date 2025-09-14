a3db777485f2dc06ef0f94879da0012a14f41758
engsr6982 <pwxg286704@163.com>
2025-09-15 01:09:23 +0800

    feat: add default configurations and thread enum

--- src/config/EdmGlobalConfig.cc
+++ src/config/EdmGlobalConfig.cc
@@ -1,8 +1,12 @@
 #include "EdmGlobalConfig.h"
 
+#include "EasyDownloadManager.h"
+#include "ui/main/MainWindow.h"
 #include "utils/StringUtils.h"
 
 #include <qdir.h>
+#include <qmessagebox.h>
+#include <qstandardpaths.h>
 
 #include <magic_enum/magic_enum.hpp>
 
@@ -31,39 +35,35 @@ constexpr auto kProxyUser      = "proxy/user";                // string
 constexpr auto kProxyPassword  = "proxy/password";            // string
 
 
-int EdmGlobalConfig::getThreadCount() const { return settings_.value(kThreadCount, 8).toInt(); }
+int EdmGlobalConfig::getThreadCount() const {
+    return settings_.value(kThreadCount, static_cast<int>(kDefaultThreadCount)).toInt();
+}
 
 void EdmGlobalConfig::setThreadCount(int count) {
     settings_.setValue(kThreadCount, count);
     settings_.sync();
 }
 
-unsigned int EdmGlobalConfig::getBandwidthLimit() const { return settings_.value(kBandwidthLimit, 0).toInt(); }
+unsigned int EdmGlobalConfig::getBandwidthLimit() const {
+    return settings_.value(kBandwidthLimit, kDefaultBandwidthLimit).toInt();
+}
 
 void EdmGlobalConfig::setBandwidthLimit(unsigned int limit) {
     settings_.setValue(kBandwidthLimit, limit);
     settings_.sync();
 }
 
-QString EdmGlobalConfig::getUserAgent() const {
-    return settings_
-        .value(
-            kUserAgent,
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 "
-            "Safari/537.36 Edg/140.0.0.0"
-        )
-        .toString();
-}
+QString EdmGlobalConfig::getUserAgent() const { return settings_.value(kUserAgent, kDefaultUserAgent).toString(); }
 
 void EdmGlobalConfig::setUserAgent(QString const& userAgent) {
     settings_.setValue(kUserAgent, userAgent);
     settings_.sync();
 }
 
-
-inline constexpr auto        kProxyTypeNone = magic_enum::enum_name(EdmGlobalConfig::ProxyType::None);
 EdmGlobalConfig::ProxyConfig EdmGlobalConfig::getProxyConfig() const {
-    auto const typeStr = settings_.value(kProxyType, string_utils::stringview2qstring(kProxyTypeNone)).toString();
+    static constexpr auto None = magic_enum::enum_name(EdmGlobalConfig::ProxyType::None);
+
+    auto const typeStr = settings_.value(kProxyType, string_utils::stringview2qstring(None)).toString();
 
     ProxyConfig cfg{};
     cfg.type_ = magic_enum::enum_cast<ProxyType>(string_utils::qstring2stringview(typeStr)).value_or(ProxyType::None);
@@ -85,34 +85,57 @@ void EdmGlobalConfig::setProxyConfig(ProxyConfig const& config) {
     settings_.sync();
 }
 
-QString EdmGlobalConfig::getSaveDir() const {
-    return settings_.value(kSaveDir, QDir::homePath() + "/Downloads").toString();
-}
+QString EdmGlobalConfig::getSaveDir() const { return settings_.value(kSaveDir, getDefaultSaveDir()).toString(); }
 
 void EdmGlobalConfig::setSaveDir(QString const& dir) {
     settings_.setValue(kSaveDir, dir);
     settings_.sync();
 }
 
-QString EdmGlobalConfig::getTempDir() const { return settings_.value(kTempDir, QDir::tempPath()).toString(); }
+QString EdmGlobalConfig::getTempDir() const { return settings_.value(kTempDir, getDefaultTempDir()).toString(); }
 
 void EdmGlobalConfig::setTempDir(QString const& dir) {
     settings_.setValue(kTempDir, dir);
     settings_.sync();
 }
 
-bool EdmGlobalConfig::canAutoStart() const { return settings_.value(kAutoStart, false).toBool(); }
+bool EdmGlobalConfig::canAutoStart() const { return settings_.value(kAutoStart, kDefaultAutoStart).toBool(); }
 
 void EdmGlobalConfig::setAutoStart(bool autoStart) {
     settings_.setValue(kAutoStart, autoStart);
     settings_.sync();
 }
 
-bool EdmGlobalConfig::canShowDownloadCompleteDialog() const { return settings_.value(kShowComplete, true).toBool(); }
+bool EdmGlobalConfig::canShowDownloadCompleteDialog() const {
+    return settings_.value(kShowComplete, kDefaultShowComplete).toBool();
+}
 
 void EdmGlobalConfig::setShowDownloadCompleteDialog(bool show) {
     settings_.setValue(kShowComplete, show);
     settings_.sync();
+}
+
+
+QString EdmGlobalConfig::getDefaultSaveDir() {
+    return QStandardPaths::writableLocation(QStandardPaths::DownloadLocation);
+}
+QString EdmGlobalConfig::getDefaultTempDir() {
+    QString temp = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/edm";
+    QDir    dir;
+    if (!dir.exists(temp)) {
+        if (!dir.mkpath(temp)) {
+            auto msgBox = new QMessageBox(
+                QMessageBox::Warning,
+                "Error",
+                "Failed to create temp directory",
+                QMessageBox::Ok,
+                EasyDownloadManager::getOrNewInstance().getMainWindow()
+            );
+            msgBox->setAttribute(Qt::WA_DeleteOnClose);
+            msgBox->show();
+        }
+    }
+    return temp;
 }
 
 
--- src/config/EdmGlobalConfig.h
+++ src/config/EdmGlobalConfig.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <magic_enum/magic_enum.hpp>
 #include <qsettings.h>
 #include <qtclasshelpermacros.h>
 
@@ -16,6 +17,7 @@ public:
     [[nodiscard]] static EdmGlobalConfig& instance();
 
     // 线程
+    enum class AvailableThreads { k4 = 4, k8 = 8, k16 = 16, k32 = 32 };
     int  getThreadCount() const;
     void setThreadCount(int count);
 
@@ -28,7 +30,7 @@ public:
     void    setUserAgent(QString const& userAgent);
 
     // 代理
-    enum class ProxyType { None, Http, Https, Socks4, Socks4a, Socks5, Socks5h };
+    enum class ProxyType { None = 0, Http, Https, Socks4, Socks4a, Socks5, Socks5h };
     struct ProxyConfig {
         ProxyType              type_{ProxyType::None};
         std::optional<QString> host_{std::nullopt};
@@ -54,6 +56,21 @@ public:
     // 下载完成对话框
     bool canShowDownloadCompleteDialog() const;
     void setShowDownloadCompleteDialog(bool show);
+
+
+public:
+    static inline constexpr auto kDefaultThreadCount    = AvailableThreads::k8;
+    static inline constexpr auto kDefaultBandwidthLimit = 0;
+    static inline constexpr auto kDefaultUserAgent    = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                                                        "(KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0";
+    static inline constexpr auto kDefaultProxyType    = ProxyType::None;
+    static inline constexpr auto kDefaultProxyHost    = "127.0.0.1";
+    static inline constexpr auto kDefaultProxyPort    = 1080;
+    static inline constexpr auto kDefaultAutoStart    = false;
+    static inline constexpr auto kDefaultShowComplete = true;
+
+    static QString getDefaultSaveDir();
+    static QString getDefaultTempDir();
 };
 
 } // namespace edm
